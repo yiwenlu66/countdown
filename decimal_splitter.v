c2d5bb3b021d16e007f9da96e8e5e61d866f795a
Peter Lu <mcfloundinho@gmail.com>
2016-12-20 14:16:19 +0800

    add decimal splitter

---
+++ decimal_splitter.v
@@ -0,0 +1,16 @@
+/*
+ * Split a 2-digit decimal number into digits.
+ */
+
+module decimal_splitter(
+    input [6:0] dec,
+    output reg [3:0] tens,
+    output reg [3:0] units
+    );
+
+always @ * begin
+    tens = dec / 10;
+    units = dec % 10;
+end
+
+endmodule
---
+++ tb_decimal_splitter.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module tb_decimal_splitter;
+
+reg [6:0] dec;
+wire [3:0] tens;
+wire [3:0] units;
+
+decimal_splitter d (
+    .dec(dec),
+    .tens(tens),
+    .units(units)
+    );
+
+initial begin
+    dec = 25;
+    #100 dec = 56;
+    #100 dec = 83;
+end
+
+endmodule
