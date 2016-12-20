0ec1c962458005ff1fd7261d20a259a0f5f0235a
Peter Lu <mcfloundinho@gmail.com>
2016-12-20 15:38:42 +0800

    change two_four_decoder to active low

--- two_four_decoder.v
+++ two_four_decoder.v
@@ -1,3 +1,5 @@
+/* ACTIVE LOW */
+
 module two_four_decoder(
     input [1:0] in,
     output out_0, out_1, out_2, out_3
@@ -7,10 +9,10 @@ reg [3:0] out;
 
 always @ * begin
     case (in)
-        0: out = 4'b0001;
-        1: out = 4'b0010;
-        2: out = 4'b0100;
-        3: out = 4'b1000;
+        0: out = 4'b1110;
+        1: out = 4'b1101;
+        2: out = 4'b1011;
+        3: out = 4'b0111;
     endcase
 end
 
