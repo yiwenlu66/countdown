43bd98c724b388357d5c493d5d2a506be93665fb
Peter Lu <mcfloundinho@gmail.com>
2016-12-20 15:25:20 +0800

    add scan display

---
+++ scan_display.v
@@ -0,0 +1,26 @@
+/*
+ * Scan through the numbers with the given clock;
+ * output a number with its index at a time.
+ */
+
+module scan_display(
+    input scan_clk,          // recommended: ~250Hz
+    input [3:0] num_in_0,
+    input [3:0] num_in_1,
+    input [3:0] num_in_2,
+    input [3:0] num_in_3,
+    output reg [3:0] num_out,
+    output reg [1:0] index = 0
+    );
+
+always @ (posedge scan_clk) begin
+    index = index + 1;
+    case (index)
+        0: num_out = num_in_0;
+        1: num_out = num_in_1;
+        2: num_out = num_in_2;
+        3: num_out = num_in_3;
+    endcase
+end
+
+endmodule
---
+++ tb_scan_display.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+module tb_scan_display;
+
+reg scan_clk = 0;
+wire [3:0] num_out;
+wire [1:0] index;
+
+scan_display s (
+    .scan_clk(scan_clk),
+    .num_in_0(4),
+    .num_in_1(5),
+    .num_in_2(6),
+    .num_in_3(7),
+    .num_out(num_out),
+    .index(index)
+    );
+
+initial begin
+    forever #100 scan_clk = !scan_clk;
+end
+
+endmodule
