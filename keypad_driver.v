1e3ed067dedee7cd8b5620bdc306eb79170548bc
Peter Lu <mcfloundinho@gmail.com>
2016-12-20 20:31:44 +0800

    output key in keypad driver

--- keypad_driver.v
+++ keypad_driver.v
@@ -2,7 +2,8 @@ module keypad_driver(
     input scan_clk,
     input row_1, row_2, row_3, row_4,
     output col_1, col_2, col_3, col_4,
-    output reg keydown
+    output reg keydown,
+    output reg [3:0] key
     );
 
 reg [1:0] col = 0;
@@ -14,7 +15,13 @@ two_four_decoder decoder (
 
 always @ (posedge scan_clk) begin
     col = col + 1;
-    if (!row_1 || !row_2 || !row_3 || !row_4) keydown = 1;
+    if (!row_1 || !row_2 || !row_3 || !row_4) begin
+        keydown = 1;
+        if (!row_1) key = col;
+        else if (!row_2) key = col + 4;
+        else if (!row_3) key = col + 8;
+        else key = col + 12;
+    end
     else keydown = 0;
 end
 
