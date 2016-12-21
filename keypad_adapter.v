7169376e02aaea51052e558e55277b9e2cf4886c
Peter Lu <mcfloundinho@gmail.com>
2016-12-21 20:33:56 +0800

    debounce keyboard

--- keypad_adapter.v
+++ keypad_adapter.v
@@ -1,9 +1,9 @@
 /*
  * Keypad layout: name[function](id)
- * 0(13)            7(9)    4(5)    1(1)
- * F[start](14)     8(10)   5(6)    2(2)
- * E[clear](15)     9(11)   6(7)    3(3)
- * D[confirm](12)   C(8)    B(4)    A(0)
+ * 0         (12)   7(8)    4(4)    1(0)
+ * F[start]  (13)   8(9)    5(5)    2(1)
+ * E[clear]  (14)   9(10)   6(6)    3(2)
+ * D[confirm](15)   C(11)   B(7)    A(3)
  */
 
 module keypad_adapter(
@@ -16,22 +16,22 @@ module keypad_adapter(
 always @ * begin
 
     case (key_id)
-        1: num = 1;
-        2: num = 2;
-        3: num = 3;
-        5: num = 4;
-        6: num = 5;
-        7: num = 6;
-        9: num = 7;
-        10: num = 8;
-        11: num = 9;
-        13: num = 0;
+        0: num = 1;
+        1: num = 2;
+        2: num = 3;
+        4: num = 4;
+        5: num = 5;
+        6: num = 6;
+        8: num = 7;
+        9: num = 8;
+        10: num = 9;
+        12: num = 0;
         default: num = 10;  // invalid
     endcase
 
-    keydown_start = (keydown && key_id == 14);
-    keydown_confirm = (keydown && key_id == 12);
-    keydown_clear = (keydown && key_id == 15);
+    keydown_start = (keydown && key_id == 13);
+    keydown_confirm = (keydown && key_id == 15);
+    keydown_clear = (keydown && key_id == 14);
     keydown_num = (keydown && num != 10);
     
 end
--- keypad_driver.v
+++ keypad_driver.v
@@ -6,23 +6,36 @@ module keypad_driver(
     output reg [3:0] key
     );
 
-reg [1:0] col = 0;
+reg [3:0] col = 0, row = 0;     // preserve enough bits for arithmetic operations
+reg [3:0] active_row, active_col;
 
 two_four_decoder decoder (
     .in(col),
     .out_0(col_1), .out_1(col_2), .out_2(col_3), .out_3(col_4)
     );
 
+wire [3:0] rows;
+assign rows = {row_4, row_3, row_2, row_1};
+
 always @ (posedge scan_clk) begin
-    col = col + 1;
-    if (!row_1 || !row_2 || !row_3 || !row_4) begin
-        keydown = 1;
-        if (!row_1) key = col;
-        else if (!row_2) key = col + 4;
-        else if (!row_3) key = col + 8;
-        else key = col + 12;
+
+    if (!keydown) begin
+        for (row = 0; row < 4; row = row + 1) begin
+            if (!rows[row]) begin
+                keydown = 1;
+                active_row = row;
+                active_col = col;
+                key = (row << 2) + col;
+            end
+        end
     end
-    else keydown = 0;
+
+    else begin
+        if (col == active_col && rows[active_row]) keydown = 0;
+    end
+
+    col = (col + 1) % 4;
+
 end
 
 endmodule
